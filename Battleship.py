89e9b25e3c10a8685db464b03120e93009be17a6
newt <hi@newty.dev>
2021-09-18 21:48:27 +0100

    Save scores to a csv file

--- Battleship.py
+++ Battleship.py
@@ -2,28 +2,36 @@ from Board import Board
 import os
 import json
 import re
+import csv
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-cls()
+cls() # Clear anything that may currently be in the terminal
 
+currentDir = os.path.dirname(os.path.realpath(__file__))
 # Load settings.json
-with open(os.path.dirname(os.path.realpath(__file__)) + '/settings.json') as f:
+with open(currentDir + '/settings.json') as f:
 	settings = json.load(f)
 
 board = Board(8, settings)
 torpedos = settings.get('torpedos')
 score = 0
 
-# Ready the board and render it
+# Ready the board
 board.spawnShips()
+
+# Find out the player's name/alias
+playerName = input('Before you play... what would you like to be called?\n')
+cls()
+
+# Render the initial board
 board.render()
 
 while torpedos != 0:
 	while True:
 		try:
-			coordinates = input('\nPlease enter the coordinates you would like to shoot at in an (x,y) format. You have %i torpedos remaining.\n' % (torpedos))
+			coordinates = input('\n%s, please enter the coordinates you would like to shoot at in an (x,y) format. You have %i torpedos remaining.\n' % (playerName, torpedos))
 			# Remove any brackets
 			coordinates = re.sub(r'[\(\)]', '', coordinates)
 			coordinates = coordinates.split(',')
@@ -61,6 +69,11 @@ while torpedos != 0:
 print("""
 Game Over!
 
-Your score was: %i
+Your score was: %i (:
 """ % (score))
 board.renderResultBoard()
+
+# Save the score to the csv file
+with open(currentDir + '/scores.csv', 'w') as f:
+	writer = csv.writer(f, delimiter=',')
+	writer.writerow([playerName, score])
